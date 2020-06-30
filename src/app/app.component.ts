7e23bffe0b65b21f16c7e3d697fb84f8bac38da1
Isoboy <isoboy.k@gmail.com>
2020-06-30 11:39:00 +0500

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  arr = [1, 2, 3, 4, 5];
+}
